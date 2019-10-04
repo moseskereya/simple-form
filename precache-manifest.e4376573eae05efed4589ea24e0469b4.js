23ea9aff970aa48dece50f6eb1fb33411dd1738d
MOSES <MOSESLAZARO2019@outlook.com>
2019-10-04 05:56:42 +0300

    Updates

---
+++ precache-manifest.e4376573eae05efed4589ea24e0469b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f3ec7a976ed803a0106ceed92466f3f",
+    "url": "/simple-form/index.html"
+  },
+  {
+    "revision": "591f21431d818cb35972",
+    "url": "/simple-form/static/css/main.f2e430be.chunk.css"
+  },
+  {
+    "revision": "4ca2f2f65ce7548570aa",
+    "url": "/simple-form/static/js/2.6668bdb0.chunk.js"
+  },
+  {
+    "revision": "591f21431d818cb35972",
+    "url": "/simple-form/static/js/main.fe331180.chunk.js"
+  },
+  {
+    "revision": "e6d25969daaa2584931c",
+    "url": "/simple-form/static/js/runtime-main.7a81bb11.js"
+  }
+]);
